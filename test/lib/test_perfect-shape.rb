25e5f524ea3b8637462d24117eb46f95ca5bee8d
Andy Maleh <andy.am@gmail.com>
2021-12-05 16:47:48 -0500

    Added one more test to Arc for updating attributes

--- test/lib/test_perfect-shape.rb
+++ test/lib/test_perfect-shape.rb
@@ -27,6 +27,25 @@ describe PerfectShape do
       _(arc.start).must_equal 0
       _(arc.extent).must_equal 360
     end
+    
+    it 'updates attributes' do
+      arc = PerfectShape::Arc.new
+      arc.type = :chord
+      arc.x = 2
+      arc.y = 3
+      arc.width = 50
+      arc.height = 60
+      arc.start = 30
+      arc.extent = 90
+
+      _(arc.type).must_equal :chord
+      _(arc.y).must_equal 3
+      _(arc.y).must_equal 3
+      _(arc.width).must_equal 50
+      _(arc.height).must_equal 60
+      _(arc.start).must_equal 30
+      _(arc.extent).must_equal 90
+    end
   end
   
 #   specification('Arc') {
@@ -38,22 +57,6 @@ describe PerfectShape do
 #       }
 #
 #       scenario('updating attributes') {
-#         arc = PerfectShape::Arc.new
-#         arc.type = :chord
-#         arc.x = 2
-#         arc.y = 3
-#         arc.width = 50
-#         arc.height = 60
-#         arc.start = 30
-#         arc.extent = 90
-#
-#         fact { arc.type == :chord }
-#         fact { arc.y == 3 }
-#         fact { arc.y == 3 }
-#         fact { arc.width == 50 }
-#         fact { arc.height == 60 }
-#         fact { arc.start == 30 }
-#         fact { arc.extent == 90 }
 #       }
 #     }
 #
