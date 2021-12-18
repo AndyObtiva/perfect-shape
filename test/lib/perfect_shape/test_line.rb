2ffbb1a360aee1558dcdadba6ca8319733970521
Andy Maleh <andy.am@gmail.com>
2021-12-18 16:23:26 -0500

    Line updates attributes test

--- test/lib/perfect_shape/test_line.rb
+++ test/lib/perfect_shape/test_line.rb
@@ -46,52 +46,26 @@ describe PerfectShape do
       _(shape.center_x).must_equal nil
       _(shape.center_y).must_equal nil
     end
+    
+    it 'raises an error if constructed with one point only' do
+      # TODO
+    end
 
-#     it 'updates attributes' do
-#       shape = PerfectShape::Line.new
-#       shape.points = [[200, 150], [270, 170], [250, 220], [220, 190], [200, 200], [180, 170]]
-#
-#       _(shape.points).must_equal [[200, 150], [270, 170], [250, 220], [220, 190], [200, 200], [180, 170]]
-#       _(shape.min_x).must_equal 180
-#       _(shape.min_y).must_equal 150
-#       _(shape.max_x).must_equal 270
-#       _(shape.max_y).must_equal 220
-#       _(shape.width).must_equal 90
-#       _(shape.height).must_equal 70
-#       _(shape.center_x).must_equal 180 + 45
-#       _(shape.center_y).must_equal 150 + 35
-#
-#       shape.points = [200, 150]
-#
-#       _(shape.points).must_equal [[200, 150]]
-#       _(shape.min_x).must_equal 200
-#       _(shape.min_y).must_equal 150
-#       _(shape.max_x).must_equal 200
-#       _(shape.max_y).must_equal 150
-#       _(shape.width).must_equal 0
-#       _(shape.height).must_equal 0
-#       _(shape.center_x).must_equal 200
-#       _(shape.center_y).must_equal 150
-#
-#       shape.points << [270, 170]
-#       shape.points << [250, 220]
-#       shape.points << [220, 190]
-#       shape.points << [200, 200]
-#       shape.points << [180, 170]
-#       shape.points.delete [180, 170]
-#       shape.points << [180, 170]
-#
-#       _(shape.points).must_equal [[200, 150], [270, 170], [250, 220], [220, 190], [200, 200], [180, 170]]
-#       _(shape.min_x).must_equal 180
-#       _(shape.min_y).must_equal 150
-#       _(shape.max_x).must_equal 270
-#       _(shape.max_y).must_equal 220
-#       _(shape.width).must_equal 90
-#       _(shape.height).must_equal 70
-#       _(shape.center_x).must_equal 180 + 45
-#       _(shape.center_y).must_equal 150 + 35
-#     end
-#
+    it 'updates attributes' do
+      shape = PerfectShape::Line.new
+      shape.points = [[200, 150], [270, 170]]
+
+      _(shape.points).must_equal [[200, 150], [270, 170]]
+      _(shape.min_x).must_equal 200
+      _(shape.min_y).must_equal 150
+      _(shape.max_x).must_equal 270
+      _(shape.max_y).must_equal 170
+      _(shape.width).must_equal 70
+      _(shape.height).must_equal 20
+      _(shape.center_x).must_equal 200 + 35
+      _(shape.center_y).must_equal 150 + 10
+    end
+
 #     it 'contains point in center' do
 #       shape = PerfectShape::Line.new(points: [[200, 150], [270, 170], [250, 220], [220, 190], [200, 200], [180, 170]])
 #       point = [shape.center_x, shape.center_y]
