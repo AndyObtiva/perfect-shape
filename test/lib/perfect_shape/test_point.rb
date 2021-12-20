3d6d8a50636d05e5ea8eac5c30a5afbbba3a02ad
Andy Maleh <andy.am@gmail.com>
2021-12-20 14:18:39 -0500

    Update Point attributes

--- test/lib/perfect_shape/test_point.rb
+++ test/lib/perfect_shape/test_point.rb
@@ -50,21 +50,23 @@ describe PerfectShape do
       _(shape.center_y).must_equal 0
     end
 
-#     it 'updates attributes' do
-#       shape = PerfectShape::Point.new
-#       shape.points = [[200, 150], [270, 170]]
-#
-#       _(shape.points).must_equal [[200, 150], [270, 170]]
-#       _(shape.min_x).must_equal 200
-#       _(shape.min_y).must_equal 150
-#       _(shape.max_x).must_equal 270
-#       _(shape.max_y).must_equal 170
-#       _(shape.width).must_equal 70
-#       _(shape.height).must_equal 20
-#       _(shape.center_x).must_equal 200 + 35
-#       _(shape.center_y).must_equal 150 + 10
-#     end
-#
+    it 'updates attributes' do
+      shape = PerfectShape::Point.new
+      shape.x = 200
+      shape.y = 150
+
+      _(shape.x).must_equal 200
+      _(shape.y).must_equal 150
+      _(shape.min_x).must_equal 200
+      _(shape.min_y).must_equal 150
+      _(shape.max_x).must_equal 200
+      _(shape.max_y).must_equal 150
+      _(shape.width).must_equal 0
+      _(shape.height).must_equal 0
+      _(shape.center_x).must_equal 200
+      _(shape.center_y).must_equal 150
+    end
+
 #     it 'returns distance between two points' do
 #       distance = PerfectShape::Point.distance([0, 0], [100, 0])
 #
