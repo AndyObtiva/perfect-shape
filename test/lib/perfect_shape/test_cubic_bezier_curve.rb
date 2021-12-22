e118e5add7e8572b75a5e8c9a758d2df7e3be6c3
Andy Maleh <andy.am@gmail.com>
2021-12-22 13:39:32 -0500

    Test for update CubicBezierCurve attributes

--- test/lib/perfect_shape/test_cubic_bezier_curve.rb
+++ test/lib/perfect_shape/test_cubic_bezier_curve.rb
@@ -33,59 +33,59 @@ describe PerfectShape do
       _(shape.center_y).must_equal 150 + 35
     end
 
-#     it 'constructs with defaults' do
-#       shape = PerfectShape::CubicBezierCurve.new
-#
-#       _(shape.points).must_equal []
-#       _(shape.min_x).must_equal nil
-#       _(shape.min_y).must_equal nil
-#       _(shape.max_x).must_equal nil
-#       _(shape.max_y).must_equal nil
-#       _(shape.width).must_equal nil
-#       _(shape.height).must_equal nil
-#       _(shape.center_x).must_equal nil
-#       _(shape.center_y).must_equal nil
-#     end
-#
-#     it 'updates attributes' do
-#       shape = PerfectShape::CubicBezierCurve.new
-#       shape.points = [[200, 150], [270, 220], [180, 170]]
-#
-#       _(shape.points).must_equal [[200, 150], [270, 220], [180, 170]]
-#       _(shape.min_x).must_equal 180
-#       _(shape.min_y).must_equal 150
-#       _(shape.max_x).must_equal 270
-#       _(shape.max_y).must_equal 220
-#       _(shape.width).must_equal 90
-#       _(shape.height).must_equal 70
-#       _(shape.center_x).must_equal 180 + 45
-#       _(shape.center_y).must_equal 150 + 35
-#
-#       shape.points = [[201, 152], [270, 220], [180, 170]]
-#
-#       _(shape.points).must_equal [[201, 152], [270, 220], [180, 170]]
-#       _(shape.min_x).must_equal 180
-#       _(shape.min_y).must_equal 152
-#       _(shape.max_x).must_equal 270
-#       _(shape.max_y).must_equal 220
-#       _(shape.width).must_equal 90
-#       _(shape.height).must_equal 68
-#       _(shape.center_x).must_equal 180 + 45
-#       _(shape.center_y).must_equal 152 + 34
-#
-#       shape.points << shape.points.pop
-#
-#       _(shape.points).must_equal [[201, 152], [270, 220], [180, 170]]
-#       _(shape.min_x).must_equal 180
-#       _(shape.min_y).must_equal 152
-#       _(shape.max_x).must_equal 270
-#       _(shape.max_y).must_equal 220
-#       _(shape.width).must_equal 90
-#       _(shape.height).must_equal 68
-#       _(shape.center_x).must_equal 180 + 45
-#       _(shape.center_y).must_equal 152 + 34
-#     end
-#
+    it 'constructs with defaults' do
+      shape = PerfectShape::CubicBezierCurve.new
+
+      _(shape.points).must_equal []
+      _(shape.min_x).must_equal nil
+      _(shape.min_y).must_equal nil
+      _(shape.max_x).must_equal nil
+      _(shape.max_y).must_equal nil
+      _(shape.width).must_equal nil
+      _(shape.height).must_equal nil
+      _(shape.center_x).must_equal nil
+      _(shape.center_y).must_equal nil
+    end
+
+    it 'updates attributes' do
+      shape = PerfectShape::CubicBezierCurve.new
+      shape.points = [[200, 150], [230, 160], [270, 220], [180, 170]]
+
+      _(shape.points).must_equal [[200, 150], [230, 160], [270, 220], [180, 170]]
+      _(shape.min_x).must_equal 180
+      _(shape.min_y).must_equal 150
+      _(shape.max_x).must_equal 270
+      _(shape.max_y).must_equal 220
+      _(shape.width).must_equal 90
+      _(shape.height).must_equal 70
+      _(shape.center_x).must_equal 180 + 45
+      _(shape.center_y).must_equal 150 + 35
+
+      shape.points = [[201, 150], [230, 161], [270, 220], [180, 170]]
+
+      _(shape.points).must_equal [[201, 150], [230, 161], [270, 220], [180, 170]]
+      _(shape.min_x).must_equal 180
+      _(shape.min_y).must_equal 150
+      _(shape.max_x).must_equal 270
+      _(shape.max_y).must_equal 220
+      _(shape.width).must_equal 90
+      _(shape.height).must_equal 70
+      _(shape.center_x).must_equal 180 + 45
+      _(shape.center_y).must_equal 150 + 35
+
+      shape.points << shape.points.pop
+
+      _(shape.points).must_equal [[201, 150], [230, 161], [270, 220], [180, 170]]
+      _(shape.min_x).must_equal 180
+      _(shape.min_y).must_equal 150
+      _(shape.max_x).must_equal 270
+      _(shape.max_y).must_equal 220
+      _(shape.width).must_equal 90
+      _(shape.height).must_equal 70
+      _(shape.center_x).must_equal 180 + 45
+      _(shape.center_y).must_equal 150 + 35
+    end
+
 #     it 'equals another quadratic bezier curve' do
 #       shape = PerfectShape::CubicBezierCurve.new(points: [[200, 150], [270, 220], [180, 170]])
 #       shape2 = PerfectShape::CubicBezierCurve.new(points: [200, 150, 270, 220, 180, 170])
