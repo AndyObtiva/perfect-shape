e445c262989d833cbe88b029edd8be2851d30434
Andy Maleh <andy.am@gmail.com>
2022-01-07 08:43:22 -0500

    CompositeShape updates attributes

--- test/lib/perfect_shape/test_composite_shape.rb
+++ test/lib/perfect_shape/test_composite_shape.rb
@@ -36,5 +36,24 @@ describe PerfectShape do
       _(shape.center_x).must_equal nil
       _(shape.center_y).must_equal nil
     end
+    
+    it 'updates attributes' do
+      shapes = []
+      shapes << PerfectShape::Square.new(x: 20, y: 15, length: 100)
+      shapes << PerfectShape::Circle.new(x: 120, y: 115, diameter: 100)
+
+      shape = PerfectShape::CompositeShape.new
+      shape.shapes = shapes
+
+      _(shape.shapes).must_equal shapes
+      _(shape.min_x).must_equal 20
+      _(shape.min_y).must_equal 15
+      _(shape.max_x).must_equal 120 + 100
+      _(shape.max_y).must_equal 115 + 100
+      _(shape.width).must_equal 120 + 100 - 20
+      _(shape.height).must_equal 115 + 100 - 15
+      _(shape.center_x).must_equal 20 + (120 + 100 - 20) / 2.0
+      _(shape.center_y).must_equal 15 + (115 + 100 - 15) / 2.0
+    end
   end
 end
